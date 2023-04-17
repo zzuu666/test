6da86a566d6325c7a1751e1fe8920bd05144b73b
JHSN <qjtjhsn@gmail.com>
2023-04-17 16:20:15 +0800

    chore: add shell script to generate pull requests

---
+++ gen.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+DOMAIN=github.com
+ORIGIN=origin
+REPO=NotJHSN/test
+DEFAULT_BRANCH=master
+TOTAL=5
+
+set -ex
+
+function create_pr() {
+  git checkout $DEFAULT_BRANCH
+  git branch -D "$2" || echo branch not found
+  git checkout -b "$2" $ORIGIN/$DEFAULT_BRANCH
+  date >>"$2".txt
+  git add .
+  git commit -m "test: update $2.txt"
+  git push -f "$1" "$2"
+}
+
+if [ -n "$2" ]; then
+  TOTAL=$2
+fi
+
+INIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git fetch $ORIGIN $DEFAULT_BRANCH
+for ((i = 1; i <= TOTAL; i++)); do
+  create_pr $ORIGIN "pr$i"
+done
+git checkout $INIT_BRANCH
+
+set +x
+for ((i = 1; i <= TOTAL; i++)); do
+  echo "https://$DOMAIN/$REPO/pull/new/pr$i"
+done
